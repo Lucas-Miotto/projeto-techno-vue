417b46b46f5804703cd2c871ceeabce99da07053
Lucas Miotto <latmiotto@gmail.com>
2023-05-05 16:09:45 -0300

    Controle do carrinho, alerta e mensageria, comprar estoque e navegação por produto atualizando url.

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ const vm = new Vue({
     produtos: [],
     produto: false,
     carrinho: [],
+    carrinhoAtivo: false,
+    mensagemAlerta: "Item adicionado",
+    alertaAtivo: false,
   },
   filters: {
     numeroPreco(valor) {
@@ -49,10 +52,14 @@ const vm = new Vue({
     fecharModal({ target, currentTarget }) {
       if (target === currentTarget) this.produto = false;
     },
+    clickForaCarrinho({ target, currentTarget }) {
+      if (target === currentTarget) this.carrinhoAtivo = false;
+    },
     adicionarItem() {
       this.produto.estoque--;
       const { id, nome, preco } = this.produto;
       this.carrinho.push({ id, nome, preco });
+      this.alerta(`${nome} adicionado ao carrinho.`);
     },
     removerItem(index) {
       this.carrinho.splice(index, 1);
@@ -61,14 +68,38 @@ const vm = new Vue({
       if (window.localStorage.carrinho)
         this.carrinho = JSON.parse(window.localStorage.carrinho);
     },
+    compararEstoque() {
+      const items = this.carrinho.filter(({ id }) => id === this.produto.id);
+      this.produto.estoque -= items.length;
+    },
+    alerta(mensagem) {
+      this.mensagemAlerta = mensagem;
+      this.alertaAtivo = true;
+      setTimeout(() => {
+        this.alertaAtivo = false;
+      }, 1500);
+    },
+    router() {
+      const hash = document.location.hash;
+      if (hash) this.fetchProduto(hash.replace("#", ""));
+    },
   },
   watch: {
+    produto() {
+      document.title = this.produto.nome || "Techno";
+      const hash = this.produto.id || "";
+      history.pushState(null, null, `#${hash}`);
+      if (this.produto) {
+        this.compararEstoque();
+      }
+    },
     carrinho() {
       window.localStorage.carrinho = JSON.stringify(this.carrinho);
     },
   },
   created() {
     this.fetchProdutos();
+    this.router();
     this.checarLocalStorage();
   },
 });
